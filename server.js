23976da7f59b8dc28efdc7853ef83edc2b50271e
heylch <liuchuhan1993@gmail.com>
2018-12-04 20:25:25 -0500

    modify

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(app.express.static(__dirname + '/public'));
+app.use(app.express.static(__dirname + '/public/project'));
 
 var port = process.env.PORT || 3000;
 
