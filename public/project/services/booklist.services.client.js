ee85a975f814c22e897d02e70227f8f2cf725083
crazynature <cui.jia@husky.neu.edu>
2018-12-05 20:13:39 -0500

    add booklist service

---
+++ public/project/services/booklist.services.client.js
@@ -0,0 +1,73 @@
+(function () {
+    angular
+        .module("ITBook")
+        .service("booklistService", booklistService);
+
+
+    function booklistService($http) {
+
+        var api =  {
+            "createBooklistForUser": createBooklistForUser,
+            "findBooklistById": findBooklistById,
+            "findBooklistByBooklistName":findBooklistByBooklistName,
+            "findAllBooklistsByUser": findAllBooklistsByUser,
+            "updateBooklist": updateBooklist,
+            "deleteBooklist": deleteBooklist,
+            "addBookToBooklist":addBookToBooklist,
+            "getAllBooksFromBooklist":getAllBooksFromBooklist,
+            "removeBookFromBooklist": removeBookFromBooklist
+        };
+
+        return api;
+
+
+        function findBooklistById(booklistId) {
+            var url = "/projectapi/booklist/" + booklistId;
+            return $http.get(url);
+        }
+
+        function findAllBooklistsByUser(userId) {
+            // console.log(userId);
+            var url = "/projectapi/user/" + userId + "/booklist";
+            return $http.get(url);
+        }
+
+        function createBooklistForUser(userId, booklist) {
+            var url = "/projectapi/user/" + userId + "/booklist";
+            return $http.post(url,booklist);
+        }
+
+        function findBooklistByBooklistName(booklistname) {
+            var url = "/projectapi/booklist?booklistname="+booklistname;
+            return $http.get(url);
+        }
+
+        function updateBooklist(booklistId,booklist){
+            var url = "/projectapi/booklist/" + booklistId;
+            return $http.put(url,booklist);
+        }
+
+        function deleteBooklist(booklistId){
+            var url = "/projectapi/booklist/" + booklistId;
+            return $http.delete(url);
+        }
+
+        function addBookToBooklist(booklistId, bookId) {
+            // console.log(booklistId);
+            var url = "/projectapi/booklist/" + booklistId + "/book/" + bookId;
+            return $http.put(url);
+        }
+
+        function getAllBooksFromBooklist(booklistId) {
+            var url = "/projectapi/booklist/" + booklistId + "/book/";
+            return $http.get(url);
+        }
+        function removeBookFromBooklist(booklistId,bookId) {
+            var url = "/projectapi/booklist/" + booklistId + "/book/" + bookId +"/remove";
+            return $http.put(url);
+        }
+
+
+    }
+
+})();
